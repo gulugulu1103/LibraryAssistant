000e053143318d545b2d7e65a7d59ceee47c7c59
gulugulu1103 <gulugulu1103@qq.com>
2022-05-04 12:32:26 +0800

    学生结构体变量改为包含rec数组，去除链表结构

--- class.h
+++ class.h
@@ -9,14 +9,14 @@ typedef struct {
 
 typedef struct {
     // 一条记录
-    Book book;
-    char time[81];
+    Book book; // 记录的对象，书本
+    char time[81]; // 时间字符串
     int borrow;  // borrow = 1 是借出, borrow = 0 是归还
-    Rec* next;
 } Rec;
 
 typedef struct {
     // 学生
     char num[81];
-    Rec* head;
+    Rec rec[21]; // 借还记录数组
+    int recNum; // 借还记录数量
 } Stu;
