4dc6a20ee6d3ecad066eaeaf4c7ff00ffd911789
gulugulu1103 <gulugulu1103@qq.com>
2022-06-04 16:03:50 +0800

    bug fix

--- library.c
+++ library.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <corecrt.h>
 #include "class.h"
 void addBook()
 {
--- student.c
+++ student.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <corecrt.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
